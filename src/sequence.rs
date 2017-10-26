9c978d6764c389ade4e2908b30c5633d556a9ba4
Rob Donnelly <rfdonnelly@gmail.com>
2017-10-25 20:36:32 -0700

    Add sequence_from_ast
    to convert the AST into a Sequence

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,24 +1,51 @@
 use ast::Opcode;
+use ast::Node;
 
 use rand::SeedableRng;
 use rand::chacha::ChaChaRng;
 use rand::distributions::Range;
 use rand::distributions::IndependentSample;
 
+
 pub trait Sequence {
     fn next(&mut self) -> u32;
     fn last(&self) -> u32;
+}
+
+fn sequence_from_ast(node: &Node) -> Box<Sequence> {
+    match *node {
+        Node::Identifier(_) => panic!("Not supported"),
+        Node::Assignment(_, _) => panic!("Not supported"),
+        Node::Range(ref bx, ref by) => {
+            Box::new(
+                RangeSequence::new(
+                    &mut sequence_from_ast(bx),
+                    &mut sequence_from_ast(by)
+                )
+            )
+        }
+        Node::Number(x) => Box::new(Value::new(x)),
+        Node::Operation(ref bx, ref op, ref by) => {
+            Box::new(
+                Expr::new(
+                    sequence_from_ast(bx),
+                    op.clone(),
+                    sequence_from_ast(by)
+                )
+            )
+        }
+    }
 }
 
 pub struct Value {
     last: u32,
 }
 
-pub struct Expr<'a> {
+pub struct Expr {
     last: u32,
     operation: Opcode,
-    l: &'a mut Sequence,
-    r: &'a mut Sequence,
+    l: Box<Sequence>,
+    r: Box<Sequence>,
 }
 
 pub struct RangeSequence {
@@ -45,8 +72,8 @@ impl Sequence for Value {
     }
 }
 
-impl<'a> Expr<'a> {
-    pub fn new(l: &'a mut Sequence, operation: Opcode, r: &'a mut Sequence) -> Expr<'a> {
+impl<'a> Expr {
+    pub fn new(l: Box<Sequence>, operation: Opcode, r: Box<Sequence>) -> Expr {
         Expr {
             last: 0,
             operation: operation,
@@ -56,7 +83,7 @@ impl<'a> Expr<'a> {
     }
 }
 
-impl<'a> Sequence for Expr<'a> {
+impl<'a> Sequence for Expr {
     fn next(&mut self) -> u32 {
         self.last = match self.operation {
             Opcode::Add => self.l.next() + self.r.next(),
@@ -74,7 +101,7 @@ impl<'a> Sequence for Expr<'a> {
 }
 
 impl<'a> RangeSequence {
-    pub fn new(l: &'a mut Sequence, r: &'a mut Sequence) -> RangeSequence {
+    pub fn new(l: &mut Box<Sequence>, r: &mut Box<Sequence>) -> RangeSequence {
         // FIXME: Range::new may panic.
         // FIXME: Allow non-const seed
         // FIXME?: Range is from x (inclusive) to y (exclusive) so add 1 to y to make it inclusive.
@@ -104,13 +131,13 @@ mod tests {
 
     #[test]
     fn expr() {
-        let mut v0 = Value::new(1);
-        let mut v1 = Value::new(2);
+        let v0 = Box::new(Value::new(1));
+        let v1 = Box::new(Value::new(2));
 
         let mut expr = Expr::new(
-            &mut v0,
+            v0,
             Opcode::Add,
-            &mut v1,
+            v1,
         );
 
         assert_eq!(expr.next(), 3);
@@ -121,8 +148,8 @@ mod tests {
     fn range() {
         use std::collections::HashMap;
 
-        let mut v0 = Value::new(0);
-        let mut v1 = Value::new(1);
+        let mut v0: Box<Sequence> = Box::new(Value::new(0));
+        let mut v1: Box<Sequence> = Box::new(Value::new(1));
 
         let mut range = RangeSequence::new(
             &mut v0,
@@ -143,5 +170,40 @@ mod tests {
 
         assert!(num_zeros > 490 && num_zeros < 510);
         assert!(num_ones > 490 && num_ones < 510);
+    }
+
+    mod sequence_from_ast {
+        use super::super::*;
+
+        #[test]
+        fn number() {
+            let ast = Node::Number(4);
+            let mut sequence = sequence_from_ast(&ast);
+
+            assert_eq!(sequence.next(), 4);
+        }
+
+        #[test]
+        fn range() {
+            use std::collections::HashMap;
+
+            let ast = Node::Range(
+                Box::new(Node::Number(3)),
+                Box::new(Node::Number(4))
+            );
+            let mut sequence = sequence_from_ast(&ast);
+
+            let mut values = HashMap::new();
+
+            for _ in 0..10 {
+                let value = sequence.next();
+                let entry = values.entry(value).or_insert(0);
+                *entry += 1;
+                assert!(value == 3 || value == 4);
+            }
+
+            assert!(values[&3] > 0);
+            assert!(values[&4] > 0);
+        }
     }
 }
