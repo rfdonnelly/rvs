a3a67a08ce192f83a33070c85147bb6408cedf4e
Rob Donnelly <rfdonnelly@gmail.com>
2017-10-25 22:39:29 -0700

    Handle ranges up to ::std::u32::MAX

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,6 +1,7 @@
 use ast::Opcode;
 use ast::Node;
 
+use rand::Rng;
 use rand::SeedableRng;
 use rand::chacha::ChaChaRng;
 use rand::distributions::Range;
@@ -50,6 +51,8 @@ pub struct Expr {
 pub struct RangeSequence {
     last: u32,
     rng: ChaChaRng,
+    use_range: bool,
+    offset: bool,
     range: Range<u32>,
 }
 
@@ -103,19 +106,63 @@ impl<'a> RangeSequence {
     pub fn new(l: &mut Box<Sequence>, r: &mut Box<Sequence>) -> RangeSequence {
         // FIXME: Range::new may panic.
         // FIXME: Allow non-const seed
-        // FIXME?: Range is from x (inclusive) to y (exclusive) so add 1 to y to make it inclusive.
-        // Does this prevent max int (0xffffffff) from being covered?
+        let mut r = r.next();
+        let mut l = l.next();
+        let mut use_range = true;
+        let mut offset = false;
+
+        // RangeSequence is [x, y] but Range is from [x, y). Compensate for this by handling 3
+        // cases:
+        //
+        // * The range [::std::u32::MIN, ::std::u32::MAX]
+        //
+        //   Cannot use rand::distributions::Range.  Use RNG directly.
+        //
+        // * The range [x, ::std::u32::MAX]
+        //
+        //   Can use rand::distributions::Range but must adjust the range down artifically, then
+        //   re-adjust up after sampling.
+        //
+        // * All other ranges
+        //
+        //   Use rand::distributions::Range normally.
+        if r == ::std::u32::MAX {
+            if l == ::std::u32::MIN {
+                // Sample directly from RNG w/o Range
+                use_range = false;
+                r -= 1; // Prevent panic on Range::new
+            } else {
+                // Sample with Range + offset
+                offset = true;
+                l -= 1;
+                r -= 1;
+            }
+        }
+
         RangeSequence {
             last: 0,
             rng: ChaChaRng::from_seed(&[0x0000_0000]),
-            range: Range::new(l.next(), r.next() + 1),
+            offset: offset,
+            use_range: use_range,
+            range: Range::new(l, r + 1),
         }
     }
 }
 
 impl<'a> Sequence for RangeSequence {
     fn next(&mut self) -> u32 {
-        self.last = self.range.ind_sample(&mut self.rng);
+        // Should never see this case.  Could cause a panic due to overflow.
+        assert!(!(self.use_range == false && self.offset == true));
+
+        if self.use_range {
+            self.last = self.range.ind_sample(&mut self.rng);
+        } else {
+            self.last = self.rng.gen();
+        }
+
+        if self.offset {
+            self.last += 1
+        }
 
         self.last
     }
@@ -143,32 +190,84 @@ mod tests {
         assert_eq!(expr.next(), 3);
     }
 
-    #[test]
-    fn range() {
-        use std::collections::HashMap;
+    mod range {
+        use super::super::*;
 
-        let mut v0: Box<Sequence> = Box::new(Value::new(0));
-        let mut v1: Box<Sequence> = Box::new(Value::new(1));
+        #[test]
+        fn basic() {
+            use std::collections::HashMap;
 
-        let mut range = RangeSequence::new(
-            &mut v0,
-            &mut v1
-        );
+            let mut v0: Box<Sequence> = Box::new(Value::new(0));
+            let mut v1: Box<Sequence> = Box::new(Value::new(1));
 
-        let mut values = HashMap::new();
+            let mut range = RangeSequence::new(
+                &mut v0,
+                &mut v1
+                );
 
-        for _ in 0..1000 {
-            let value = range.next();
-            let entry = values.entry(value).or_insert(0);
-            *entry += 1;
-            assert!(value == 0 || value == 1);
+            let mut values = HashMap::new();
+
+            for _ in 0..1000 {
+                let value = range.next();
+                let entry = values.entry(value).or_insert(0);
+                *entry += 1;
+                assert!(value == 0 || value == 1);
+            }
+
+            let num_zeros = values[&0];
+            let num_ones = values[&1];
+
+            assert!(num_zeros > 490 && num_zeros < 510);
+            assert!(num_ones > 490 && num_ones < 510);
         }
 
-        let num_zeros = values[&0];
-        let num_ones = values[&1];
+        #[test]
+        fn max_max() {
+            use std::collections::HashMap;
 
-        assert!(num_zeros > 490 && num_zeros < 510);
-        assert!(num_ones > 490 && num_ones < 510);
+            let mut v0: Box<Sequence> = Box::new(Value::new(::std::u32::MAX - 1));
+            let mut v1: Box<Sequence> = Box::new(Value::new(::std::u32::MAX));
+            let mut sequence = RangeSequence::new(
+                &mut v0,
+                &mut v1
+            );
+
+            let mut values = HashMap::new();
+            for _ in 0..100 {
+                let value = sequence.next();
+                let entry = values.entry(value).or_insert(0);
+                *entry += 1;
+                assert!(value == ::std::u32::MAX - 1 || value == ::std::u32::MAX);
+            }
+
+            assert!(values[&(::std::u32::MAX - 1)] > 0);
+            assert!(values[&::std::u32::MAX] > 0);
+        }
+
+        #[test]
+        #[ignore]
+        fn full_range() {
+            use std::collections::HashMap;
+
+            let mut v0: Box<Sequence> = Box::new(Value::new(::std::u32::MIN));
+            let mut v1: Box<Sequence> = Box::new(Value::new(::std::u32::MAX));
+            let mut sequence = RangeSequence::new(
+                &mut v0,
+                &mut v1
+            );
+
+            let mut values = HashMap::new();
+            for _ in 0u64..0x2_0000_0000u64 {
+                let value = sequence.next();
+                if value == ::std::u32::MIN || value == ::std::u32::MAX {
+                    let entry = values.entry(value).or_insert(0);
+                    *entry += 1;
+                }
+            }
+
+            assert!(values[&::std::u32::MIN] > 0);
+            assert!(values[&::std::u32::MAX] > 0);
+        }
     }
 
     mod sequence_from_ast {
