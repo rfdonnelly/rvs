46c967eac3ca86e17013429525d9253f00eb5422
Rob Donnelly <rfdonnelly@gmail.com>
2017-10-26 21:34:31 -0700

    Add sequences_from_ast

--- src/sequences/mod.rs
+++ src/sequences/mod.rs
@@ -2,6 +2,8 @@ pub mod value;
 pub mod expr;
 pub mod range;
 
+use std::collections::HashMap;
+
 use ast::Node;
 
 pub use self::value::Value;
@@ -11,6 +13,24 @@ pub use self::range::RangeSequence;
 pub trait Sequence {
     fn next(&mut self) -> u32;
     fn last(&self) -> u32;
+}
+
+pub fn sequences_from_ast(assignments: Vec<Box<Node>>) -> HashMap<String, Box<Sequence>> {
+    let mut sequences = HashMap::new();
+
+    for assignment in assignments {
+        if let Node::Assignment(ref lhs, ref rhs) = *assignment {
+            let mut identifier: String = "".into();
+
+            if let Node::Identifier(ref x) = **lhs {
+                identifier = x.clone();
+            }
+
+            sequences.insert(identifier, sequence_from_ast(&rhs));
+        }
+    }
+
+    sequences
 }
 
 pub fn sequence_from_ast(node: &Node) -> Box<Sequence> {
@@ -70,6 +90,37 @@ mod tests {
 
             assert!(values[&3] > 0);
             assert!(values[&4] > 0);
+        }
+    }
+
+    mod sequences_from_ast {
+        use super::super::*;
+
+        use std::collections::hash_map::Entry::Occupied;
+
+        #[test]
+        fn basic() {
+            let assignments = vec![
+                Box::new(Node::Assignment(
+                    Box::new(Node::Identifier("a".into())),
+                    Box::new(Node::Number(5))
+                )),
+                Box::new(Node::Assignment(
+                    Box::new(Node::Identifier("b".into())),
+                    Box::new(Node::Number(6))
+                )),
+            ];
+
+            let mut sequences = sequences_from_ast(assignments);
+
+            assert!(sequences.contains_key("a"));
+            if let Occupied(mut entry) = sequences.entry("a".into()) {
+                assert_eq!(entry.get_mut().next(), 5);
+            }
+            assert!(sequences.contains_key("b"));
+            if let Occupied(mut entry) = sequences.entry("b".into()) {
+                assert_eq!(entry.get_mut().next(), 6);
+            }
         }
     }
 }
