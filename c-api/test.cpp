cbe8989f831eccd9b70039eb50a8d1a469caf98a
Rob Donnelly <rfdonnelly@gmail.com>
2017-11-04 22:05:32 -0700

    Fix C API test

--- c-api/test.cpp
+++ c-api/test.cpp
@@ -2,24 +2,24 @@
 #include <cassert>
 
 extern "C" {
-    uint32_t parse(const char* s);
-    uint32_t lookup(const char* name, uint32_t* handle_ptr);
-    uint32_t next(uint32_t handle, uint32_t* result_ptr);
+    uint32_t sequence_parse(const char* s);
+    uint32_t sequence_find(const char* name, uint32_t* handle_ptr);
+    uint32_t sequence_next(uint32_t handle, uint32_t* result_ptr);
 }
 
 int main() {
     uint32_t err = 0;
 
-    err = parse("a=5;");
+    err = sequence_parse("a=5;");
     assert(err == 0);
 
     uint32_t handle = 0;
-    err = lookup("a", &handle);
+    err = sequence_find("a", &handle);
     assert(err == 0);
     assert(handle == 1);
 
     uint32_t result = 0;
-    err = next(handle, &result);
+    err = sequence_next(handle, &result);
     assert(err == 0);
     assert(result == 5);
 }
