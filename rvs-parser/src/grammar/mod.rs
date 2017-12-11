2a88af4a0ecf3595a3cd6966b5484f701ccd7395
Rob Donnelly <rfdonnelly@gmail.com>
2017-12-10 20:28:56 -0800

    Decrease visibility of grammar rules

--- rvs-parser/src/grammar/mod.rs
+++ rvs-parser/src/grammar/mod.rs
@@ -4,39 +4,60 @@ include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
 mod tests {
     use super::*;
 
-    mod atom {
-        use super::*;
-
-        #[test]
-        fn good() {
-            assert_eq!(atom("(5)", &mut RequirePaths::new()), Ok(Box::new(Node::Number(5))));
-            assert_eq!(atom("5", &mut RequirePaths::new()), Ok(Box::new(Node::Number(5))));
-            assert_eq!(atom("0xa", &mut RequirePaths::new()), Ok(Box::new(Node::Number(10))));
-            assert_eq!(atom("0xaf", &mut RequirePaths::new()), Ok(Box::new(Node::Number(0xaf))));
-        }
-
-        #[test]
-        fn bad() {
-            assert!(atom("(5))", &mut RequirePaths::new()).is_err());
-            assert!(atom("(5", &mut RequirePaths::new()).is_err());
-        }
-    }
-
     mod expr {
         use super::*;
 
         #[test]
         fn good() {
-            assert_eq!(expr("1+2", &mut RequirePaths::new()), Ok(
-                Box::new(Node::BinaryOperation(
-                    Box::new(Node::Number(1)),
-                    BinaryOpcode::Add,
-                    Box::new(Node::Number(2))
-                ))
-            ));
+            assert_eq!(items("a = (5);", &mut RequirePaths::new()).unwrap(), vec![Item::Single(Box::new(
+                        Node::Assignment(
+                            Box::new(Node::Identifier("a".into())),
+                            Box::new(Node::Number(5))
+                        )
+                    ))]);
+            assert_eq!(items("a = 5;", &mut RequirePaths::new()).unwrap(), vec![Item::Single(Box::new(
+                        Node::Assignment(
+                            Box::new(Node::Identifier("a".into())),
+                            Box::new(Node::Number(5))
+                        )
+                    ))]);
+            assert_eq!(items("a = 0xa;", &mut RequirePaths::new()).unwrap(), vec![Item::Single(Box::new(
+                        Node::Assignment(
+                            Box::new(Node::Identifier("a".into())),
+                            Box::new(Node::Number(10))
+                        )
+                    ))]);
+            assert_eq!(items("a = 0xaf;", &mut RequirePaths::new()).unwrap(), vec![Item::Single(Box::new(
+                        Node::Assignment(
+                            Box::new(Node::Identifier("a".into())),
+                            Box::new(Node::Number(0xaf))
+                        )
+                    ))]);
+        }
 
-            assert_eq!(format!("{:?}", expr("1+2*3", &mut RequirePaths::new())),
-            "Ok(BinaryOperation(Number(1), Add, BinaryOperation(Number(2), Mul, Number(3))))");
+        #[test]
+        fn bad() {
+            assert!(items("a = (5));", &mut RequirePaths::new()).is_err());
+            assert!(items("a = (5;", &mut RequirePaths::new()).is_err());
+        }
+
+        #[test]
+        fn operations() {
+            assert_eq!(items("a = 1+2;", &mut RequirePaths::new()).unwrap(), vec![
+                       Item::Single(
+                           Box::new(Node::Assignment(
+                                   Box::new(Node::Identifier("a".into())),
+                                   Box::new(Node::BinaryOperation(
+                                           Box::new(Node::Number(1)),
+                                           BinaryOpcode::Add,
+                                           Box::new(Node::Number(2))
+                                           ))
+                                   ))
+                           )
+            ]);
+
+            assert_eq!(format!("{:?}", items("a = 1+2*3;", &mut RequirePaths::new()).unwrap()),
+                "[Single(Assignment(Identifier(\"a\"), BinaryOperation(Number(1), Add, BinaryOperation(Number(2), Mul, Number(3)))))]");
         }
     }
 
@@ -45,18 +66,18 @@ mod tests {
 
         #[test]
         fn good() {
-            assert!(identifier("a", &mut RequirePaths::new()).is_ok());
-            assert!(identifier("a_", &mut RequirePaths::new()).is_ok());
-            assert!(identifier("__", &mut RequirePaths::new()).is_ok());
-            assert!(identifier("_0", &mut RequirePaths::new()).is_ok());
-            assert!(identifier("a::B", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a_ = 0;", &mut RequirePaths::new()).is_ok());
+            assert!(items("__ = 0;", &mut RequirePaths::new()).is_ok());
+            assert!(items("_0 = 0;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a::B = 0;", &mut RequirePaths::new()).is_ok());
         }
 
         #[test]
         fn bad() {
-            assert!(identifier("a-b", &mut RequirePaths::new()).is_err());
-            assert!(identifier("0b", &mut RequirePaths::new()).is_err());
-            assert!(identifier("1_", &mut RequirePaths::new()).is_err());
+            assert!(items("a-b = 0;", &mut RequirePaths::new()).is_err());
+            assert!(items("0b = 0;", &mut RequirePaths::new()).is_err());
+            assert!(items("1_ = 0;", &mut RequirePaths::new()).is_err());
         }
     }
 
@@ -65,16 +86,16 @@ mod tests {
 
         #[test]
         fn good() {
-            assert!(number("5", &mut RequirePaths::new()).is_ok());
-            assert!(number("5_", &mut RequirePaths::new()).is_ok());
-            assert!(number("5_6", &mut RequirePaths::new()).is_ok());
-            assert!(number("5__6", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 5;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 5_;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 5_6;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 5__6;", &mut RequirePaths::new()).is_ok());
         }
 
         #[test]
         fn bad() {
-            assert!(number("a", &mut RequirePaths::new()).is_err());
-            assert!(number("_5", &mut RequirePaths::new()).is_err());
+            assert!(items("a = a;", &mut RequirePaths::new()).is_err());
+            assert!(items("a = _5;", &mut RequirePaths::new()).is_err());
         }
     }
 
@@ -83,17 +104,17 @@ mod tests {
 
         #[test]
         fn good() {
-            assert!(hex_number("0xa5E4", &mut RequirePaths::new()).is_ok());
-            assert!(hex_number("0XA5", &mut RequirePaths::new()).is_ok());
-            assert!(hex_number("0X_A5", &mut RequirePaths::new()).is_ok());
-            assert!(hex_number("0XA_5", &mut RequirePaths::new()).is_ok());
-            assert!(hex_number("0XA__5", &mut RequirePaths::new()).is_ok());
-            assert!(hex_number("0XA5_", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0xa5E4;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0XA5;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0X_A5;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0XA_5;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0XA__5;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = 0XA5_;", &mut RequirePaths::new()).is_ok());
         }
 
         #[test]
         fn bad() {
-            assert!(hex_number("0xg5", &mut RequirePaths::new()).is_err());
+            assert!(items("a = 0xg5;", &mut RequirePaths::new()).is_err());
         }
     }
 
@@ -102,12 +123,12 @@ mod tests {
 
         #[test]
         fn good() {
-            assert!(assignment("a=5;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a=5;", &mut RequirePaths::new()).is_ok());
         }
 
         #[test]
         fn ast() {
-            assert_eq!(assignment("a=5;", &mut RequirePaths::new()), Ok(
+            assert_eq!(items("a=5;", &mut RequirePaths::new()).unwrap(), vec![
                 Item::Single(
                     Box::new(
                         Node::Assignment(
@@ -116,17 +137,17 @@ mod tests {
                         )
                     )
                 )
-            ));
+            ]);
         }
 
         #[test]
         fn bad() {
-            assert!(assignment("a=5", &mut RequirePaths::new()).is_err());
+            assert!(items("a=5", &mut RequirePaths::new()).is_err());
         }
 
         #[test]
         fn with_enum() {
-            assert!(assignment("a = Enum::Value;", &mut RequirePaths::new()).is_ok());
+            assert!(items("a = Enum::Value;", &mut RequirePaths::new()).is_ok());
         }
     }
 
@@ -145,7 +166,7 @@ mod tests {
 
         #[test]
         fn ast() {
-            assert_eq!(items(" a  = // comment0\n5 ; // comment1\nb=6;", &mut RequirePaths::new()), Ok(vec![
+            assert_eq!(items(" a  = // comment0\n5 ; // comment1\nb=6;", &mut RequirePaths::new()).unwrap(), vec![
                 Item::Single(
                     Box::new(Node::Assignment(
                         Box::new(Node::Identifier("a".into())),
@@ -158,7 +179,7 @@ mod tests {
                         Box::new(Node::Number(6))
                     )),
                 )
-            ]));
+            ]);
         }
     }
 
@@ -167,17 +188,21 @@ mod tests {
 
         #[test]
         fn ast() {
-            assert_eq!(range("[1,2]", &mut RequirePaths::new()), Ok(
-                    Box::new(
-                        Node::Function(
-                            Function::Range,
-                            vec![
-                                Box::new(Node::Number(1)),
-                                Box::new(Node::Number(2))
-                            ]
-                        )
-                    )
-                ));
+            assert_eq!(items("a = [1,2];", &mut RequirePaths::new()).unwrap(), vec![
+                       Item::Single(
+                           Box::new(Node::Assignment(
+                                   Box::new(Node::Identifier("a".into())),
+                                   Box::new(
+                                       Node::Function(
+                                           Function::Range,
+                                           vec![
+                                           Box::new(Node::Number(1)),
+                                           Box::new(Node::Number(2))
+                                           ]
+                                           )
+                                       )
+                                   )))
+            ]);
         }
     }
 }
