f3cd4ef32d43d30f8f6d25e0386d27802fb46f16
Rob Donnelly <rfdonnelly@gmail.com>
2017-12-29 22:08:25 -0800

    Add Result type

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,11 @@ pub mod error;
 pub use rvs_parser::error::ParseError;
 pub use error::TransformError;
 pub use error::Error;
+pub use error::Result;
 
 use types::Context;
 
-pub fn parse(s: &str, context: &mut Context) -> Result<(), Error> {
+pub fn parse(s: &str, context: &mut Context) -> Result<()> {
     let items = rvs_parser::parse(s, &mut context.search_path)?;
     context.transform_items(&items).unwrap();
     Ok(())
