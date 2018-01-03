01d2fca7b78f1e8199683536be8fe039484996e6
Rob Donnelly <rfdonnelly@gmail.com>
2018-01-02 22:57:04 -0800

    Remove unnecessary scope
    No longer necessary now that variables are wrapped in a RefCell.

--- tests/parse.rs
+++ tests/parse.rs
@@ -7,15 +7,11 @@ fn basic() {
         "a=[0,1];\nb=2;"
         ).unwrap();
 
-    {
-        let a = model.get_variable_by_name("a").unwrap();
-        let result = a.borrow_mut().next();
-        assert!(result == 0 || result == 1);
-    }
+    let a = model.get_variable_by_name("a").unwrap();
+    let result = a.borrow_mut().next();
+    assert!(result == 0 || result == 1);
 
-    {
-        let b = model.get_variable_by_name("b").unwrap();
-        let result = b.borrow_mut().next();
-        assert_eq!(result, 2);
-    }
+    let b = model.get_variable_by_name("b").unwrap();
+    let result = b.borrow_mut().next();
+    assert_eq!(result, 2);
 }
