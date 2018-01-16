80b1822c328175048bd43b45d6454d600dad1dd0
Rob Donnelly <rfdonnelly@gmail.com>
2018-01-15 16:39:52 -0800

    Accept &str or String

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -4,8 +4,10 @@ use std::io;
 use std::rc::Rc;
 use std::cell::RefCell;
 
-pub fn expr_to_var(expr: &str) -> rvs::Result<Rc<RefCell<Box<rvs::Variable>>>> {
-    let s = format!("variable = {};", expr);
+pub fn expr_to_var<S>(expr: S) -> rvs::Result<Rc<RefCell<Box<rvs::Variable>>>>
+    where S: AsRef<str>
+{
+    let s = format!("variable = {};", expr.as_ref());
     let model = rvs::parse(Default::default(), &s)?;
     match model.get_variable_by_name("variable") {
         Some(variable) => {
